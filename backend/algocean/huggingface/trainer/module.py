bb1be78a26770cad93598b81803acf5e7cc8ee78
Vivona <vivonasg@gmail.com>
2022-09-04 10:35:26 -0400

    trained hf model

--- backend/algocean/huggingface/trainer/module.py
+++ backend/algocean/huggingface/trainer/module.py
@@ -14,6 +14,8 @@ from algocean.utils import dict_put
 from datasets.utils.py_utils import asdict, unique_values
 import datetime
 import pyarrow
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
 
 from ocean_lib.models.data_nft import DataNFT
 import fsspec
@@ -52,14 +54,17 @@ class TrainerModule(BaseModule):
 
     def load_state(self):
         self.load_dataset()
-        self.load_pipeline()
         self.load_model()
+        self.load_optimizer()
         self.load_metric()
+        self.load_schedular()
         
     def load_dataset(self):
         dataset_class = self.get_object('huggingface.dataset.module.DatasetModule')
         override = dict(dataset=self.config.get('dataset'), load_dataset=True, client=self.client)
         self.dataset = dataset_class(override=override).dataset
+        self.load_pipeline()
+        self.dataloaders = self.get_dataloaders()
         # st.write(self.dataset.dataset)
 
     def split(self, split):
@@ -71,21 +76,28 @@ class TrainerModule(BaseModule):
         pipeline_class = self.import_object(path=kwargs.get('module'))
         pipeline = getattr(pipeline_class, kwargs['init']['fn'])(**kwargs['init'].get('kwargs', {}))
         self.pipeline = pipeline
-
-
-        st.write(self.dataset)
+        
         def pipeline_function(examples):
             return pipeline(examples["text"], padding="max_length", truncation=True)
         
         for split, dataset in self.dataset.items():
-            self.dataset[split] = self.dataset[split].map(pipeline_function, batched=True)
-
+            dataset = dataset.map(pipeline_function, batched=True)
+            dataset = dataset.remove_columns(["text"])
+            dataset = dataset.rename_column("label", "labels")    
+            dataset.set_format('torch')
+            self.dataset[split] = dataset
+    
+    @property
+    def device(self):
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        return device
 
     def load_model(self):
         kwargs = self.config.get('model')
         model_class = self.import_object(path=kwargs.get('module'))
         model = getattr(model_class, kwargs['init']['fn'])(**kwargs['init'].get('kwargs', {}))
         self.model = model
+        self.model.to(self.device)
 
 
     def load_metric(self):
@@ -108,7 +120,10 @@ class TrainerModule(BaseModule):
         
         raise Exception('Please specify the number of epochs in the config via num_epochs, or epochs')
 
-
+    @property
+    def num_training_steps(self):
+        return self.num_epochs * len(self.dataloader['train'])
+        
 
     def load_optimizer(self):
         kwargs = self.config.get('optimizer')
@@ -117,32 +132,46 @@ class TrainerModule(BaseModule):
 
     @property
     def batch_size(self):
-        return self.config.get('batch_size') 
+        return self.config.get('batch_size', 8) 
 
     @property
     def shuffle(self):
         return self.config.get('shuffle', True)
+    
+
     def get_dataloaders(self):
         self.dataloader = {}
         for split in self.dataset.keys():
-            self.dataloader[split] = DataLoader(small_train_dataset, shuffle=, batch_size=self.batch_size)
-
+            self.dataloader[split] = DataLoader(self.dataset[split], shuffle=self.shuffle, batch_size=self.batch_size)
+        return self.dataloader
     def load_schedular(self):
-        num_training_steps = self.num_epochs * len(self.dataloader['train'])
-        lr_scheduler = get_scheduler(
-            name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-        )
+        kwargs = self.config['schedular']
+        get_scheduler = self.import_object(kwargs['module'])
+        self.scheduler = get_scheduler( optimizer=self.optimizer, num_training_steps=self.num_training_steps, **kwargs['params'])
+
+    def train(self):
+        progress_bar = tqdm(range(self.num_training_steps))
+        self.model.train()
+        for epoch in range(self.num_epochs):
+            for batch in self.dataloader['train']:
+                st.write({k: v.shape for k, v in batch.items()})
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                outputs = self.model(**batch)
+                st.write(outputs)
+                loss = outputs.loss
+                loss.backward()
+                self.optimizer.step()
+                self.scheduler.step()
+                self.optimizer.zero_grad()
+                # progress_bar.update(1)
+
+
 
 if __name__ == '__main__':
     import streamlit as st
     import numpy as np
     from algocean.utils import *
-
-
-
-       
     module = TrainerModule()
-    st.write(module.split('train'))
-    # st.write(module.dataset)
-    # st.write(module.dataset)
+
+    module.train()
     
